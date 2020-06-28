ce619efadb007dfce82150aa37523f6fe48b16b9
boxhock <jonas@smartcontract.com>
2020-06-28 10:05:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Chainlink adapter for Google Finance
+
+For the time being, this adapter has only been confirmed working with Docker.
+
+## Output
+
+```json
+{
+    "jobRunID":"test123",
+    "data":{
+        "symbol":"INDEXFTSE:UKX",
+        "companyName":"6,159.30",
+        "ticker":6159.3,
+        "last":6159.3,
+        "open":6147.14,
+        "high":6262.13,
+        "low":6147.14,
+        "result":6159.3
+    },
+    "result":6159.3,
+    "statusCode":200
+}
+```
---
+++ adapter.js
@@ -0,0 +1,34 @@
+const { Requester, Validator } = require('@chainlink/external-adapter')
+const google = require('boxhock_google-finance-data')
+
+const commonKeys = {
+    N225: 'INDEXNIKKEI:NI225',
+    FTSE: 'INDEXFTSE:UKX'
+}
+
+const customParams = {
+    base: ['base', 'from', 'asset']
+}
+
+const createRequest = (input, callback) => {
+    const validator = new Validator(callback, input, customParams)
+    const jobRunID = validator.validated.id
+    let symbol = validator.validated.data.base.toUpperCase()
+    if (commonKeys[symbol]) {
+        symbol = commonKeys[symbol]
+    }
+
+    google.getSymbol(symbol).then(data => {
+        const status = 200
+        const response = {
+            data,
+            status
+        }
+        response.data.result = Requester.validateResultNumber(response.data, ['ticker'])
+        callback(status, Requester.success(jobRunID, response))
+    }).catch(err => {
+        callback(500, Requester.errored(jobRunID, err.message))
+    })
+}
+
+module.exports.createRequest = createRequest
